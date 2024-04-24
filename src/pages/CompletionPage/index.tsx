aadd0c127ff4384d482df59ba8d2e33015d13b8a
hsk-kr <hsk.coder@gmail.com>
2024-04-24 22:36:00 +0100

    fix: CompletionPage histories could be undefined

--- src/pages/CompletionPage/index.tsx
+++ src/pages/CompletionPage/index.tsx
@@ -16,6 +16,7 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 import useHistory from '../../hooks/useHistory';
 import dayjs from 'dayjs';
 import { History } from '../../types/history';
+import FullscreenLoading from '../../components/FullscreenLoading';
 
 const CanvasWrapper = styled.div<{
   width: number;
@@ -65,7 +66,7 @@ function CompletionPage() {
   };
 
   useEffect(() => {
-    if (histories.length <= 0) return;
+    if (!histories?.length) return;
     const idx = getSearchParamAsNumber('id');
 
     const history =
@@ -76,49 +77,54 @@ function CompletionPage() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [histories]);
 
+  const loading = history === null;
+
   return (
     <DefaultTemplate disablePadding bgGreen>
+      <FullscreenLoading useRandomInitialLoadingTime visible={loading} />
       <Box bgColor="green.500" minH="100svh">
-        <Container maxW="container.md" p={4} pt={[4, 12]}>
-          <Flex flexDir="column" gap={8}>
-            <Flex flexDir="column" gap={1} color="white" alignItems="center">
-              <ChakraText
-                fontSize={['2xl', '3xl']}
-                textTransform="uppercase"
-                textDecoration="underline"
-                onClick={navigateToLearnPage}
-                cursor="pointer"
-              >
-                LEARN MORE
-              </ChakraText>
-              <ChakraText
-                fontSize={['3xl', '4xl']}
-                textTransform="uppercase"
-                fontWeight="bold"
-              >
-                Congratulations!
-              </ChakraText>
+        {!loading && (
+          <Container maxW="container.md" p={4} pt={[4, 12]}>
+            <Flex flexDir="column" gap={8}>
+              <Flex flexDir="column" gap={1} color="white" alignItems="center">
+                <ChakraText
+                  fontSize={['2xl', '3xl']}
+                  textTransform="uppercase"
+                  textDecoration="underline"
+                  onClick={navigateToLearnPage}
+                  cursor="pointer"
+                >
+                  LEARN MORE
+                </ChakraText>
+                <ChakraText
+                  fontSize={['3xl', '4xl']}
+                  textTransform="uppercase"
+                  fontWeight="bold"
+                >
+                  Congratulations!
+                </ChakraText>
+              </Flex>
+              {history && (
+                <>
+                  <Certificate
+                    lessonTitle={history.lessonTitle}
+                    completedDate={history.completedDate}
+                  />
+                  {isDesktop && (
+                    <ChakraText
+                      fontSize="lg"
+                      color="white"
+                      textAlign="right"
+                      fontWeight="bold"
+                    >
+                      Hover your mouse on the certificate to download it.
+                    </ChakraText>
+                  )}
+                </>
+              )}
             </Flex>
-            {history && (
-              <>
-                <Certificate
-                  lessonTitle={history.lessonTitle}
-                  completedDate={history.completedDate}
-                />
-                {isDesktop && (
-                  <ChakraText
-                    fontSize="lg"
-                    color="white"
-                    textAlign="right"
-                    fontWeight="bold"
-                  >
-                    Hover your mouse on the certificate to download it.
-                  </ChakraText>
-                )}
-              </>
-            )}
-          </Flex>
-        </Container>
+          </Container>
+        )}
       </Box>
     </DefaultTemplate>
   );
