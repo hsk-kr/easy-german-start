956ee6058fbed1de1f2e372999d583fe1a9a2737
hsk-kr <hsk.coder@gmail.com>
2024-04-24 23:19:45 +0100

    build: use loadEnv instead of dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: `/${process.env.VITE_BASE_URL}/`,
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return { base: `/${env.VITE_BASE_URL}/`, plugins: [react()] };
 });
